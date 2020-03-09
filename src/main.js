a2dfd6363c59d5ebd212f566af8adb95930c29f4
kickbuttowski80 <pazpaz25@gmail.com>
2020-03-08 17:45:42 -0700

    added firebase and register the app into firebase and added vue-textarea-autosize dependency

--- src/main.js
+++ src/main.js
@@ -4,7 +4,31 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 
+// import * as firebase from 'firebase';
+// more specific 
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+import TextareaAutosize from 'vue-textarea-autosize'
+
+require('dotenv').config() 
 Vue.config.productionTip = false
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_API_KEY ,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: "vue-calendar-it",
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId 
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore()
+export default  db ;
+
+Vue.use(TextareaAutosize) 
 
 new Vue({
   router,
